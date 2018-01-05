2e695f780cf9bda926d03fe32f9cd2edd13bd09b
conjugateprior <conjugateprior@users.noreply.github.com>
2018-01-05 17:36:45 -0500

    More tests and documentation. (We pass CRAN).

---
+++ R/topicdict-package.R
@@ -0,0 +1,26 @@
+#' Bara et al's Abortion Debate Corpus
+#'
+#' A corpus of speeches from an Abortion debate in the UK House
+#' of Commons, studied by Bara et al. 2007.  The unit of analysis
+#' is the paragraph entry (406). Speaker and final vote metadata is included.
+#'
+#' @format A quanteda corpus
+#' \describe{
+#'   \item{speaker}{Name of speaker}
+#'   \item{vote}{Whether they voted yes, no, or abstained}
+#'   ...
+#' }
+#' @source Bara et al. 2007 Swiss Political Science Review
+"corpus_bara_para"
+
+#' topicdict: Topic Modeling Informed by a Content Analysis Dictionary
+#'
+#'
+#' The init function initializes a model from a seed dictionary and
+#' a list of documents. The train function runs a Gibbs sampler
+#' on the same data structure. The posterior function generates
+#' useful summary statistics, e.g. theta and beta matrices.
+#'
+#' @docType package
+#' @name topicdict
+NULL
--- README.md
+++ README.md
@@ -2,17 +2,14 @@
 
 The topic dictionary code, broken out into a separate repository.
 
-On most machines it's been necessary to drop a recent boost into `/usr/local/include`
+(no longer necessary: On most machines it's been necessary to drop a recent boost into `/usr/local/include`)
 
 ## TODO
 
-* Change `phi_s` representation? Currently the only thing not compiling in seeded_train.cpp
-* Confirm what's happening in the Z update for X==1
 * Complete the X updates
-* Run on macavity data
+* Put the log likelihood calculation back in
 * Add alpha back in
-* Confirm posterior works with what we hand back
 * Check for user stops in the code
-* put the log likelihood calculation back in
+* Confirm what's happening in the Z update for X==1
 
-Will Lowe. Dec 2017
+Will Lowe. Jan 2017
