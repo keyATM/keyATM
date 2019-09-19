cd7b10ff6804598030591597a7c0433f56c40e87
tomoya-sasaki <tomo1990sasaki@gmail.com>
2019-09-18 22:43:08 -0400

    up

--- R/posterior.R
+++ R/posterior.R
@@ -61,7 +61,7 @@ keyATM_output <- function(model){
 
     theta <- do.call(rbind, lapply(1:length(model$Z), posterior_z))
 
-  }else if(model$mode %in% c("basic", "tot")){
+  }else if(model$mode %in% c("basic", "tot", "ldaweight")){
     alpha <- model$alpha_iter[[length(model$alpha_iter)]]  
 
     posterior_z <- function(zvec){
