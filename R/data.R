9747b9e53afb5404c746b22d17a4ce635659bc37
Shusei Eshima <shusei.eshima@gmail.com>
2019-10-16 19:00:22 -0400

    add data to the package

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Bills data
+#'
+#'
+#' @format A list with following objects:
+#' \describe{
+#'   \item{doc_dfm}{A \code{quanteda} dfm object of 140 documents}
+#'   \item{cov}{}
+#'   \item{keywords}{}
+#'   \item{time_index}{}
+#' }
+#'
+#' @source \url{http://}
+"keyATM_data_bills"
