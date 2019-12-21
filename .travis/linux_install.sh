002e340f7eac12a8c480f552a4fc8e2701420655
Shusei <Shusei-E@users.noreply.github.com>
2019-12-21 11:20:33 -0500

    Update linux_install.sh

--- .travis/linux_install.sh
+++ .travis/linux_install.sh
@@ -1,1 +1,6 @@
-@test
+sudo add-apt-repository -y ppa:cran/poppler
+sudo sudo apt-get install -y libpoppler-cpp-dev
+mkdir -p ~/.R
+echo "CXX14FLAGS=-O3 -mtune=native -march=native -Wno-unused-variable -Wno-unused-function  -Wno-macro-redefined" >> ~/.R/Makevars
+echo "CXX14=g++ -std=c++1y -fext-numeric-literals -fPIC" >> ~/.R/Makevars
+Rscript -e 'install.packages(c("devtools", "ndjson"))'
