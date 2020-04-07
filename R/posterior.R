939db4455fd400617cee78fb7c090b62dc2b52d1
tomoya-sasaki <tomo1990sasaki@gmail.com>
2020-04-06 21:49:12 -0400

    update

--- R/posterior.R
+++ R/posterior.R
@@ -683,8 +683,7 @@ top_words_calc <- function(n, measure, show_keyword,
       for (j in 1:length(keywords_raw)) {
         if (i == j) next
         inds <- which(res[,i] %in% keywords_raw[[j]])
-        label <- paste0("[", "\U2713" ,"]"), 
-                        paste0("[", as.character(j), "]"))
+        label <- paste0("[", as.character(j), "]")
         res[inds, i] <- paste(res[inds, i], label)
       }
     }
