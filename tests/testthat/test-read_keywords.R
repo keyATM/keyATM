a42b111f0c7b98717ac102d19b840ca8aba53d4a
chainsawriot <chainsawtiney@gmail.com>
2020-07-05 21:45:47 +0200

    Fix initialization for test_read-keywords

--- tests/testthat/test-read_keywords.R
+++ tests/testthat/test-read_keywords.R
@@ -57,9 +57,10 @@ test_that("Integration",{
     dictfile <- tempfile(fileext = ".yml")
     download.file("https://raw.githubusercontent.com/koheiw/quanteda.seededlda/master/tests/data/topics.yml", dictfile)
     kw <- read_keywords(dictfile, docs = docs)
+    set.seed(1234)
     res <- keyATM(docs, no_keyword_topics = 0, keywords = kw, model = "base")
-    ## Kim is related to military
-    expect_true("kim" %in% top_words(res, n = 20, show_keyword = FALSE)[,"5_military"])
+    ## syria is related to military
+    expect_true("syria" %in% top_words(res, n = 20, show_keyword = FALSE)[,"5_military"])
     ## German is not related to military
     expect_true(!"german" %in% top_words(res, n = 20, show_keyword = FALSE)[,"5_military"])
 })
